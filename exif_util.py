39f7539a1208ff96e2fc08da5969239ce03cf0db
beepscore <support@beepscore.com>
2018-03-03 21:49:45 -0800

    In exif_util.py add code to open file. Currently this fails with # UnicodeDecodeError: 'utf-8' codec can't decode byte 0xff in position 0: invalid start byte

--- exif_util.py
+++ exif_util.py
@@ -1,5 +1,7 @@
+
 import os
-# from PIL import Image
+from pathlib import Path
+from PIL import Image
 
 
 def strip_exif(in_dir):
@@ -11,7 +13,26 @@ def strip_exif(in_dir):
     filenames = os.listdir(in_dir)
 
     for filename in filenames:
-        print(filename)
+
+        if filename.endswith('.jpg') or filename.endswith('.JPG')\
+                or filename.endswith('.jpeg') or filename.endswith('.JPEG'):
+
+            filename_base, filename_ext = os.path.splitext(filename)
+            print(filename_base, filename_ext)
+
+            # UnicodeDecodeError: 'utf-8' codec can't decode byte 0xff in position 0: invalid start byte
+            image_file = open(filename)
+
+            image = Image.open(image_file)
+            data = list(image.getdata())
+
+            # image_new doesn't contain exif metadata
+            image_new = Image.new(image.mode, image.size)
+            image_new.putdata(data)
+
+            filename_new_base = filename_base + '_no_exif'
+            filename_new = Path.joinpath(filename_new_base).with_suffix(filename_ext)
+            image_new.save(filename_new)
 
 
 if __name__ == '__main__':
